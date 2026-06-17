9836d1e27cbf1a41d56e385cee28af06edfd5f0d
Amaan Pasha <amaanpasha23@gmail.com>
2026-06-17 05:54:21 +0530

    Now we have fetch the data from the user and take his details from URL he have provided to use

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -6,6 +6,17 @@ app.use(express.json);
 
 app.post("/api/v1/pre-interview", (req, res)=>{
     const {success, data} = preInterviewBody.safeParse(req.body);
+    if(!success){
+        res.status(411).json({
+            message : "Incorrect body"
+        });
+        return
+    }
+    const githubUrl = data.github; // https://github.com/amaanpasha
+    const linkedinUrl = data.linkedin;
+    
+    const githubUsername = githubUrl.split("/").pop();
+    const linkedinUsername = linkedinUrl.split("/").pop();
 })
 
 app.listen(3001)
--- apps/backend/types.ts
+++ apps/backend/types.ts
@@ -4,3 +4,4 @@ export const preInterviewBody = z.object({
     linkedin : z.string(),
     github : z.string()
 })
+
