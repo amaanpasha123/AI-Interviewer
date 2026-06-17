7bb9cab85ac4e227d8248310c328e1f211a61693
Amaan Pasha <amaanpasha23@gmail.com>
2026-06-17 16:11:39 +0530

    Now we have added this for new

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,10 +1,11 @@
 import express from "express"
 import { preInterviewBody } from "./types";
+import axios from "axios";
 const app = express();
 
 app.use(express.json);
 
-app.post("/api/v1/pre-interview", (req, res)=>{
+app.post("/api/v1/pre-interview", async (req, res)=>{
     const {success, data} = preInterviewBody.safeParse(req.body);
     if(!success){
         res.status(411).json({
@@ -12,11 +13,15 @@ app.post("/api/v1/pre-interview", (req, res)=>{
         });
         return
     }
-    const githubUrl = data.github; // https://github.com/amaanpasha
-    const linkedinUrl = data.linkedin;
+    const githubUrl = data.github.endsWith("/") ? data.github.slice(0,-1) : data.github; // https://github.com/amaanpasha
+    const linkedinUrl = data.linkedin.endsWith("/") ? data.linkedin.slice(0,-1) : data.linkedin;
     
     const githubUsername = githubUrl.split("/").pop();
     const linkedinUsername = linkedinUrl.split("/").pop();
+
+    const userRepo = await axios.get(`https://api.github.com/${githubUsername}/repos`);
+
+
 })
 
 app.listen(3001)
