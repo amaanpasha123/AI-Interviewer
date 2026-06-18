0c620d0140ef7866549cc35313f4238563565026
Amaan Pasha <amaanpasha23@gmail.com>
2026-06-19 04:11:00 +0530

    finally remove some errors of home page

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,23 +1,20 @@
-import { useState } from "react";
 import { Form } from "./components/Form";
 import { Interview } from "./components/Interview";
 import { Result } from "./components/Result";
 import { Toaster } from "sonner";
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router"; // Cleaned up useNavigate/useState imports if unused
 
 export function App() {
-  const [page, setPage] = useState<"form" | "interview" | "result">("form");
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Form />}/>
-        <Route path="/interview/:id" element={<Interview />}/>
-        <Route path="/result/:id" element={<Result />}/>
+        <Route path="/" element={<Form />} />
+        <Route path="/interview/:id" element={<Interview />} />
+        <Route path="/result/:id" element={<Result />} />
       </Routes>
-      {page === "form" && <Form />}
-      {page === "interview" && <Interview />}
-      {page === "result" && <Result />}
+      
+      {/* Both components were displaying because these lines duplicated your router logic. Wiped them out! */}
+      
       <Toaster position="bottom-left" />
     </BrowserRouter>
   );
