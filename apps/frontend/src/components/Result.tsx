985ddbf53148cc66cbb5c576ac5c12f2cb234967
Amaan Pasha <amaanpasha23@gmail.com>
2026-06-22 05:20:02 +0530

    Start building the result end point at frontend first

--- apps/frontend/src/components/Result.tsx
+++ apps/frontend/src/components/Result.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react"
+
 export function Result(){
+
+    useEffect(()=>{
+            
+    },[]);
+
+
     return(
         <div>
-            Result
+            
         </div>
     )
 }
